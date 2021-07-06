61d8f4109e377fdffa7a2a137dcd39e239b5cefa
Елагина Карина Юрьенва <karina.elagina2013@yandex.ru>
2021-07-07 03:52:25 +0700

    Added handling of non-existent chat / user

--- src/main/java/ru/shift/chat/controller/ChatController.java
+++ src/main/java/ru/shift/chat/controller/ChatController.java
@@ -1,18 +1,25 @@
 package ru.shift.chat.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.shift.chat.exception.ChatNotFoundException;
 import ru.shift.chat.model.Chat;
 import ru.shift.chat.service.DatabaseService;
 
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @RestController
 public class ChatController {
 
     @Autowired
     DatabaseService databaseService;
+
+    @ResponseStatus(value= HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    private void noSuchElement(){}
 
     @PostMapping("/chat")
     private Chat createChat(@RequestBody Chat chat){
@@ -25,12 +32,12 @@ public class ChatController {
     }
 
     @PostMapping("/chat/enter")
-    private void enterTheChat(@RequestBody Map<String, Integer> map){
+    private void enterTheChat(@RequestBody Map<String, Integer> map) throws NoSuchElementException{
         databaseService.enterChat(map.get("userId"), map.get("chatId"));
     }
 
     @PostMapping("/chat/leave")
-    private void quitChat(@RequestBody Map<String, Integer> map){
+    private void quitChat(@RequestBody Map<String, Integer> map) throws NoSuchElementException{
         databaseService.leaveChat(map.get("userId"), map.get("chatId"));
     }
 }
