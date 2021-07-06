50d17715188cc89e591a15482f9094c19d229c3a
Egor Drovorub <drovorub.egor@gmail.com>
2021-07-06 11:42:28 +0600

    initial chatappl

--- src/main/java/ru/shift/chat/ChatApplication.java
+++ src/main/java/ru/shift/chat/ChatApplication.java
@@ -4,7 +4,10 @@ import com.google.gson.Gson;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import ru.shift.chat.model.User;
+import ru.shift.chat.service.UserService;
 
 @SpringBootApplication
 public class ChatApplication {
