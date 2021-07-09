1148b1a39c7712e3f8263893e17bd14916a767fe
Кожемякин Алексей Андреевич <aleksey.k2000@mail.ru>
2021-07-09 15:31:02 +0700

    Add new test to UserControllerTest

--- src/test/java/ru/shift/chat/UserControllerTest.java
+++ src/test/java/ru/shift/chat/UserControllerTest.java
@@ -1,5 +1,6 @@
 package ru.shift.chat;
 
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,8 +16,7 @@ import org.springframework.web.context.WebApplicationContext;
 import ru.shift.chat.model.User;
 import ru.shift.chat.service.DatabaseServiceImpl;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 @ContextConfiguration(classes = {ChatApplication.class})
 @WebAppConfiguration
@@ -32,12 +32,14 @@ public class UserControllerTest {
     @Autowired
     DatabaseServiceImpl databaseService;
 
+    private final Gson gson = new Gson();
+
     @Test
-    public void getAllTest() throws Exception{
+    public void getAllTest() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders
-        .get("/users")
-        .accept(MediaType.APPLICATION_JSON))
-        .andExpect(MockMvcResultMatchers.status().isOk());
+                .get("/users")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
     @Test
@@ -50,5 +52,38 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("firstName", is(equalTo(user.getFirstName()))))
                 .andExpect(MockMvcResultMatchers.jsonPath("lastName", is(equalTo(user.getLastName()))));
+    }
+
+    @Test
+    public void getNonExistUser() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/user/0").accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError());
+    }
+
+    @Test
+    public void saveUser() throws Exception {
+        User user = new User();
+        user.setFirstName("A");
+        user.setLastName("K");
+        mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(user))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("userId", is(not(empty()))))
+                .andExpect(MockMvcResultMatchers.jsonPath("firstName", is(equalTo("A"))))
+                .andExpect(MockMvcResultMatchers.jsonPath("lastName", is(equalTo("K"))));
+    }
+
+    @Test
+    public void saveNotValidUser() throws Exception{
+        User user = new User();
+        user.setFirstName("");
+        user.setLastName("K");
+        mockMvc.perform(MockMvcRequestBuilders.post("/user")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(gson.toJson(user))
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().is4xxClientError());
     }
 }
