8aff64ee94f28591f28e5f529dcf66fd24420b79
Кожемякин Алексей Андреевич <aleksey.k2000@mail.ru>
2021-07-09 21:11:05 +0700

    Add new test in MessageControllerTest

--- src/test/java/ru/shift/chat/MessageControllerTest.java
+++ src/test/java/ru/shift/chat/MessageControllerTest.java
@@ -13,7 +13,13 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.web.context.WebApplicationContext;
+import ru.shift.chat.model.Chat;
+import ru.shift.chat.model.Message;
 import ru.shift.chat.service.DatabaseServiceImpl;
+
+import java.time.LocalDateTime;
+
+import static org.hamcrest.Matchers.*;
 
 @ContextConfiguration(classes = {ChatApplication.class})
 @WebAppConfiguration
@@ -40,8 +46,22 @@ public class MessageControllerTest {
 
     @Test
     public void getMessageWithLifetime() throws Exception{
+        Chat chat = new Chat();
+        chat.setName("Test chat");
+        chat = databaseService.addChat(chat);
+        Message messageForDelete = new Message();
+        messageForDelete.setChat(chat);
+        messageForDelete.setText("Odd message");
+        messageForDelete.setSendTime(LocalDateTime.now().minusSeconds(1).toString());
+        messageForDelete.setLifetimeSec(1);
+        Message neededMessage = new Message();
+        neededMessage.setSendTime(LocalDateTime.now().toString());
+        neededMessage.setLifetimeSec(5);
+        neededMessage.setChat(chat);
+        neededMessage.setText("Needed message");
         mockMvc.perform(MockMvcRequestBuilders.get("/messages").accept(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-                //.andExpect(MockMvcResultMatchers.jsonPath("$.[0].", is()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(1)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].text", is(neededMessage.getText())));
     }
 }
