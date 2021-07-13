50392ab10a74ad26e7d58f39275c82231f7eda95
Кожемякин Алексей Андреевич <aleksey.k2000@mail.ru>
2021-07-14 00:52:55 +0700

    Added ExceptionHandler to AttachController

--- src/main/java/ru/shift/chat/controller/AttachController.java
+++ src/main/java/ru/shift/chat/controller/AttachController.java
@@ -1,16 +1,22 @@
 package ru.shift.chat.controller;
 
+import com.google.gson.Gson;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.shift.chat.DTO.AttachDTO;
+import ru.shift.chat.DTO.ErrorDTO;
+import ru.shift.chat.enums.ErrorCode;
 import ru.shift.chat.enums.TagsConstant;
 import ru.shift.chat.exception.ConnectionNotFoundException;
 import ru.shift.chat.service.AttachService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.NoSuchElementException;
 
 @RestController
 @Api(tags = {TagsConstant.ATTACH_TAG})
@@ -18,6 +24,27 @@ public class AttachController {
 
     @Autowired
     AttachService attachService;
+
+    @Autowired
+    Gson gson;
+
+    @ExceptionHandler({Exception.class})
+    private ResponseEntity<?> unknownError() {
+        return new ResponseEntity<>(gson.toJson(new ErrorDTO(ErrorCode.UNKNOWN_ERROR)),
+                HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({NoSuchElementException.class})
+    private ResponseEntity<?> noSuchElementError() {
+        return new ResponseEntity<>(gson.toJson(new ErrorDTO(ErrorCode.INCORRECT_ID)),
+                HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({ConnectionNotFoundException.class, IOException.class})
+    private ResponseEntity<?> connectionError(){
+        return new ResponseEntity<>(gson.toJson(new ErrorDTO(ErrorCode.INCOMPLETE_INPUT)),
+                HttpStatus.NOT_FOUND);
+    }
 
     @ApiOperation(value = "Sends a file to the general chat")
     @PostMapping(value = "/attach")
